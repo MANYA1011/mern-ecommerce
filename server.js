527834e8a7aab55146c41f9c68cf2142d9ea1767
manya <93120998+MANYA1011@users.noreply.github.com>
2024-08-15 22:44:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
 //configure env
 dotenv.config();
 
@@ -18,6 +19,9 @@ connectDB();
 const app = express();
 
 //middelwares
+const _filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));

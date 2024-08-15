9882938412741c0f8c5ca67cc320af97fb23d4fd
manya <93120998+MANYA1011@users.noreply.github.com>
2024-08-15 23:06:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ console.log(__dirname);
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,'./client/public')))
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
@@ -33,7 +33,7 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.use('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+  res.sendFile(path.join(__dirname,'./client/public/index.html'));
 });
 
 //PORT
